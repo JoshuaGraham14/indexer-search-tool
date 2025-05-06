2d081a13ad62ca53f2bfc9c28f356344953a0df9
Josh Graham <joshuajgraham002@gmail.com>
2025-05-06 16:32:18 +0100

    Add requests and soup for quotes and store in list

--- crawler.py
+++ crawler.py
@@ -1,6 +1,20 @@
 from bs4 import BeautifulSoup
+import requests
+import time
 
-soup = BeautifulSoup('''<h1>Geeks for Geeks</h1>''',
-                     "html.parser")
+quotes_list = []
+# 10 for 10 pages on site
+for i in range(10):
+    page = requests.get(f'https://quotes.toscrape.com/page/{i+1}/')
 
-print(type(soup))
+    soup = BeautifulSoup(page.content, "html.parser")
+    quotes = soup.find_all('div', class_='quote')
+
+    for quotes_div in quotes:
+        quote_text = quotes_div.find('span', class_='text').get_text()
+        quotes_list.append(quote_text)
+
+    print(quotes_list)
+    time.sleep(6) #6 seconds politness window.
+
+print(quotes_list)
